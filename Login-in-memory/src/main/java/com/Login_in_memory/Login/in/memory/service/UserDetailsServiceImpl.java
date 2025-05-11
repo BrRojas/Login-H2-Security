63a780e4ed111d5b99350484183311b9de29e12e
Braian Rojas <braianrojas95@gmail.com>
2025-05-11 19:38:55 -0300

    feat: implementar UserDetailsServiceImpl para autenticación
    fixes #3

---
+++ Login-in-memory/src/main/java/com/Login_in_memory/Login/in/memory/service/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.Login_in_memory.Login.in.memory.service;
+
+import com.Login_in_memory.Login.in.memory.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository
+                .findByName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado" + username));
+    }
+}
